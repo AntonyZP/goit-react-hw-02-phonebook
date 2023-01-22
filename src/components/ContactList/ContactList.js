252897582b9b06fe81be56caebd84da7bc45d29e
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-01-22 12:00:30 +0200

    in process

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-const ContactList = ({contacts}) => <ul>ContactList</ul>
+const ContactList = ({contacts}) => <ul>{contacts.map(({id, name, number} )=>
+    <li key = {id}>
+        <p> {name}</p>
+    </li>
+    )
+
+
+}
+</ul>
 
 
 export default ContactList;
